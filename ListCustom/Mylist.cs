e7df3410a84b7cd28e794050275beff460c7ba71
Mirgubad <migubad@code.edu.az>
2022-06-11 20:20:31 +0400

    Add project files.

---
+++ ListCustom/Mylist.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+
+namespace ListCustom
+{
+    class Mylist<T> :IEnumerable
+
+    {
+        private int _size = 0;
+        public int Capacity { get; set; }
+
+        public int Size
+        {
+            get { return _size; }
+        }
+        private T[] data;
+        public bool IsEmpty
+        {
+            get
+            {
+                return Size == 0;
+            }
+        }
+        public Mylist(int capacity = 8)
+        {
+
+            if (capacity < 1)
+            { capacity = 1; }
+            this.Capacity = capacity;
+            data = new T[capacity];
+
+        }
+
+        private void Resize()
+        {
+            T[] resized = new T[Capacity * 2];
+            for (int i = 0; i < Capacity; i++)
+            {
+                resized[i] = data[i];
+
+            }
+            data = resized;
+            Capacity = Capacity * 2;
+        }
+    public void Add(T element)
+        {
+            if (_size == Capacity)
+            {
+                Resize();
+            }  data[_size] = element;
+            _size++;    
+        }
+
+        public void Clear()
+        {
+            data = new T [Capacity];
+            _size = 0;
+
+        }
+        
+        public bool Contains(T value)
+        {
+            for (int i = 0; i < _size; i++)
+            {
+              
+                if (data[i].Equals(value))
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        public void Insert(T element,int index)
+        {
+            if (_size == Capacity)
+            {
+                Resize();
+            }
+
+            for (int i = _size; i > index; i--)
+            {
+                data[i] = data[i - 1];
+
+            }
+            data[index] = element;
+            _size++;
+        }
+        public void Remove(int index)
+        {
+            for (int i = index; i < _size-1; i++)
+            {
+                data[i] = data[i + 1];
+
+            }data[_size-1] =default (T);
+            _size--;
+        }
+        public IEnumerator GetEnumerator()
+        {
+            data.GetEnumerator();
+            foreach (var item in data)
+            {
+                if(item.GetType()!=default)
+                {
+                    yield return item;
+                }
+            }
+        }
+    
+    }
+}
---
+++ ListCustom/Program.cs
@@ -0,0 +1,23 @@
+﻿
+using System;
+using System.Collections.Generic;
+
+namespace ListCustom
+{
+    class Program
+    {   
+        static void Main(string[] args)
+        {
+            Mylist<int> mylist = new();
+            mylist.Add(12);
+            mylist.Add(12);
+            mylist.Add(12);
+            mylist.Add(4);
+            mylist.Add(5);
+             mylist.Add(3);
+            mylist.Insert (7, 0);          
+            Console.WriteLine( mylist.Contains(48));
+           
+        }
+    }
+}
